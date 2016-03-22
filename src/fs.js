3046af76d94e220a4644c75c25af0bd7bcf32ac6
Vladimir Starkov <iamstarkov@gmail.com>
2016-03-22 18:11:36 +0100

    fs: handle empty and invalid input

--- src/fs.js
+++ src/fs.js
@@ -1,3 +1,8 @@
 import globby from 'globby';
 
-export default path => globby('**/*.js', { cwd: path });
+// fs :: String -> Promise Array[String]
+const fs = path => typeof path === 'string'
+  ? globby('**/*.js', { cwd: path })
+  : Promise.reject(`\`path\` should be String, but got \`${typeof path}\``);
+
+export default fs;
