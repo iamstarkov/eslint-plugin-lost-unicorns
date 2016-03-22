f8a5cc0cb7ffe8276410b876b0396db47e96b9a1
Vladimir Starkov <iamstarkov@gmail.com>
2016-03-22 17:19:38 +0100

    rm default files

--- index.js
+++
@@ -1,3 +0,0 @@
-export default function eslintPluginLostUnicorns(input) {
-  return input;
-}
--- test.js
+++
@@ -1,8 +0,0 @@
-import test from 'ava';
-import eslintPluginLostUnicorns from './index';
-
-test('should eslintPluginLostUnicorns', (t) =>
-  t.same(eslintPluginLostUnicorns('unicorns'), 'unicorns'));
-
-test('should eslintPluginLostUnicorns invalid input', (t) =>
-  t.same(eslintPluginLostUnicorns(), undefined));
