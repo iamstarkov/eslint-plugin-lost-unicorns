c909ed9a25e6e8f8fbddbc4a0928dc62db8b08cf
Vladimir Starkov <iamstarkov@gmail.com>
2016-03-23 23:49:08 +0100

    contract: implement contrat util

---
+++ src/utils/contract.js
@@ -0,0 +1,9 @@
+import R from 'ramda';
+import { reject } from './promise-fp';
+
+const contract = R.curry((name, ctor, param) => R.unless(
+  R.is(ctor),
+  () => reject(`\`${name}\` should be \`${R.type(ctor())}\`, but got \`${R.type(param)}\``)
+)(param));
+
+export default contract;
