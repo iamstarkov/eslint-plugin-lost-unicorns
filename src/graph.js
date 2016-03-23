5f2ec8d80330125dd0c0d3f0f69844c3921a15b3
Vladimir Starkov <iamstarkov@gmail.com>
2016-03-23 18:49:27 +0100

    graph: add todos

--- src/graph.js
+++ src/graph.js
@@ -2,6 +2,9 @@ import R from 'ramda';
 import entry from './entry';
 import { all, resolve } from './utils/promise-fp';
 
+// @todo implement recursion properly
+// @todo take a look into path.base and path.resolve
+
 // graph :: String -> Promise Array[String]
 const graph = R.pipeP(resolve,
   entry,
