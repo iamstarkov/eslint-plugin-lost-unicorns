98b6cf4ab87b62af5a491c5efa3ab8ec0b490ed5
Vladimir Starkov <iamstarkov@gmail.com>
2016-03-23 11:22:48 +0100

    utils: introduce promise-fp

---
+++ src/utils/promise-fp.js
@@ -0,0 +1,8 @@
+import Promise from 'pinkie-promise';
+
+export const all = Promise.all.bind(Promise);
+export const race = Promise.race.bind(Promise);
+export const reject = Promise.reject.bind(Promise);
+export const resolve = Promise.resolve.bind(Promise);
+
+export default Promise;
