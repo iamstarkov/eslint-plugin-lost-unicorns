510a39240a8b4af3811cae563ce5c2766bba7f13
Vladimir Starkov <iamstarkov@gmail.com>
2016-03-24 00:04:38 +0100

    contract: add signature

--- src/utils/contract.js
+++ src/utils/contract.js
@@ -1,6 +1,7 @@
 import R from 'ramda';
 import { reject } from './promise-fp';
 
+// contract :: String -> Constructor -> a
 const contract = R.curry((name, ctor, param) => R.unless(
   R.is(ctor),
   () => reject(`\`${name}\` should be \`${R.type(ctor())}\`, but got \`${R.type(param)}\``)
