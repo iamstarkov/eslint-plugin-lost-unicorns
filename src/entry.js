f8eb71e38d20ddabc4788ea6824b6c68ded1bab7
Vladimir Starkov <iamstarkov@gmail.com>
2016-03-24 00:01:40 +0100

    entry: proper rejection

--- src/entry.js
+++ src/entry.js
@@ -2,12 +2,14 @@ import importList from 'import-list-from-string';
 import pify from 'pify';
 import _fs from 'fs';
 import R from 'ramda';
+import contract from './utils/contract';
 import { resolve } from './utils/promise-fp';
 
 const fs = pify(_fs);
 
 // entry :: String -> Promise Array[String]
 const entry = R.pipeP(resolve,
+  contract('path', String),
   path => fs.readFile(path, 'utf8'),
   importList,
   R.reject(R.isNil) // that should be a bug in importList implementation
