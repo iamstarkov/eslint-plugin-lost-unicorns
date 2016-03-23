9a1a76be9aef8a710c0f7788145220bec49d1828
Vladimir Starkov <iamstarkov@gmail.com>
2016-03-23 18:46:13 +0100

    graph: fix tests

--- test/graph.js
+++ test/graph.js
@@ -2,9 +2,9 @@ import test from 'ava';
 import graph from '../src/graph';
 
 const expected = [
-  'index.js',
-  'nested/yo.js',
+  './index.js',
+  './nested/yo.js',
 ];
 
-test('should list all expected files', t => graph('./fixtures/fs')
+test('should list all expected files', t => graph('./fixtures/fs/index.js')
   .then(result => t.same(expected, result)));
