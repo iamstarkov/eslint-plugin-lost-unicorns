1a3afaf4e89e0bef795ccc5f201e34d8a2373e4a
Vladimir Starkov <iamstarkov@gmail.com>
2016-03-24 00:01:59 +0100

    graph: proper rejection

--- src/graph.js
+++ src/graph.js
@@ -1,5 +1,6 @@
 import R from 'ramda';
 import entry from './entry';
+import contract from './utils/contract';
 import { all, resolve } from './utils/promise-fp';
 
 // @todo implement recursion properly
@@ -7,6 +8,7 @@ import { all, resolve } from './utils/promise-fp';
 
 // graph :: String -> Promise Array[String]
 const graph = R.pipeP(resolve,
+  contract('path', String),
   entry,
   R.map(entry),
   all
