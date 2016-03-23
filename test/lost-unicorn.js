08002e930b4eb1b29690fa67eaef7d098fd429c0
Vladimir Starkov <iamstarkov@gmail.com>
2016-03-24 00:31:41 +0100

    lost-unicorn: add tests

---
+++ test/lost-unicorn.js
@@ -0,0 +1,40 @@
+import test from 'ava';
+import lostUnicorn from '../src/lost-unicorn';
+
+const graph = [
+  './index.js',
+  './two/index.js',
+];
+
+const fs = [
+  './index.js',
+  './two/index.js',
+  './lost/unicorn.js',
+];
+
+const expected = [
+  './lost/unicorn.js',
+];
+
+test('should list all expected files', t => {
+  t.same(lostUnicorn(fs, graph), expected);
+});
+
+test('should throw on empty input', t => {
+  t.throws(() => {
+    lostUnicorn();
+  }, '`fs` should be `Array[String]`, but got `Undefined`');
+  t.throws(() => {
+    lostUnicorn(['s']);
+  }, '`graph` should be `Array[String]`, but got `Undefined`');
+});
+
+
+test('should throw on invalid input', t => {
+  t.throws(() => {
+    lostUnicorn(2);
+  }, '`fs` should be `Array[String]`, but got `Number`');
+  t.throws(() => {
+    lostUnicorn(['yo'], 2);
+  }, '`fs` should be `Array[String]`, but got `Number`');
+});
