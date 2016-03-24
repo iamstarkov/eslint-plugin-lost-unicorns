1b4915b4d1eb6dcfa792547f22bf4ce33a1faa72
Vladimir Starkov <iamstarkov@gmail.com>
2016-03-24 01:10:33 +0100

    lost-unicorn: implement empty input validation

--- src/lost-unicorn.js
+++ src/lost-unicorn.js
@@ -1,5 +1,15 @@
+import R from 'ramda';
 import diff from 'lodash.difference';
 
-const lostUnicorn = diff;
+const error = msg => { throw new Error(msg); };
+
+// isArrayOfString :: Input -> Boolean
+const isArrayOfStrings = R.both(R.is(Array), R.all(R.is(String)));
+
+const lostUnicorn = (fs, graph) => R.pipe(
+  () => R.unless(isArrayOfStrings, () => error('`fs` should be an `Array[String]`, but got `Undefined`'))(fs),
+  () => R.unless(isArrayOfStrings, () => error('`graph` should be an `Array[String]`, but got `Undefined`'))(graph),
+  () => diff(fs, graph)
+)(fs);
 
 export default lostUnicorn;
