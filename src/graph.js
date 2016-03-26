3288f9d0aa5bb270b49e2460f76f7ce6e56d431f
Vladimir Starkov <matmuchrapna@gmail.com>
2016-03-26 21:07:24 +0000

    graph: very specific wip

--- src/graph.js
+++ src/graph.js
@@ -2,15 +2,29 @@ import R from 'ramda';
 import entry from './entry';
 import contract from './utils/contract';
 import { all, resolve } from './utils/promise-fp';
-import { basename } from 'path';
+import { normalize, join, basename, dirname, relative } from 'path';
 
 
 // graph :: String -> Promise Array[String]
 const graph = root => {
-  const filesList = [ './' + basename(root)];
-  return entry(root).then(imports => {
-    const filesListWithImports = filesList.concat(imports);
-    return filesListWithImports;
+  const dir = dirname(root);
+  const rootList = [ './' + basename(root)];
+
+  return entry(root).then(firstLvlImports => {
+    const firstLvlList = rootList.concat(firstLvlImports);
+    // console.log(
+    //   firstLvlImports,
+    //   firstLvlImports.map(i => join(dir, i))
+    // );
+    const importLvl1Promises = firstLvlImports.map(i => join(dir, i)).map(entry);
+    return all(importLvl1Promises)
+      .then(R.flatten)
+      .then(R.map(normalize))
+      .then(R.map(i => './' + join('./first/' + i)))
+      .then(secondLvlImports => {
+        const secondLvlList = firstLvlList.concat(secondLvlImports);
+        return secondLvlList;
+    });
   });
 };
 
