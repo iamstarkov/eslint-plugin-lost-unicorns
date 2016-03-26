e0b243b2e63fe9366c580f7cfd34278e5da45bae
Vladimir Starkov <iamstarkov@gmail.com>
2016-03-26 22:40:13 +0100

    graph: update code to not add leading dots

--- src/graph.js
+++ src/graph.js
@@ -8,24 +8,26 @@ import { normalize, join, basename, dirname, relative } from 'path';
 // graph :: String -> Promise Array[String]
 const graph = root => {
   const dir = dirname(root);
-  const rootList = [ './' + basename(root)];
+  const rootList = [ basename(root)];
 
-  return entry(root).then(firstLvlImports => {
-    const firstLvlList = rootList.concat(firstLvlImports);
-    // console.log(
-    //   firstLvlImports,
-    //   firstLvlImports.map(i => join(dir, i))
-    // );
-    const importLvl1Promises = firstLvlImports.map(i => join(dir, i)).map(entry);
-    return all(importLvl1Promises)
-      .then(R.flatten)
-      .then(R.map(normalize))
-      .then(R.map(i => './' + join('./first/' + i)))
-      .then(secondLvlImports => {
-        const secondLvlList = firstLvlList.concat(secondLvlImports);
-        return secondLvlList;
+  return entry(root)
+    .then(R.map(normalize))
+    .then(firstLvlImports => {
+      const firstLvlList = rootList.concat(firstLvlImports);
+      // console.log(
+      //   firstLvlImports,
+      //   firstLvlImports.map(i => join(dir, i))
+      // );
+      const importLvl1Promises = firstLvlImports.map(i => join(dir, i)).map(entry);
+      return all(importLvl1Promises)
+        .then(R.flatten)
+        .then(R.map(normalize))
+        .then(R.map(i => join('./first/' + i)))
+        .then(secondLvlImports => {
+          const secondLvlList = firstLvlList.concat(secondLvlImports);
+          return secondLvlList;
+      });
     });
-  });
 };
 
 export default graph;
