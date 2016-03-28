dca0cfca5af6233fb6250a09e9e84cbf211229c4
Vladimir Starkov <iamstarkov@gmail.com>
2016-03-28 23:23:59 +0200

    graph: add basic plus case

--- test/graph.js
+++ test/graph.js
@@ -1,16 +1,29 @@
 import test from 'ava';
 import graph from '../src/graph';
 
-const expected = [
-  'index.js',
-  'first/index.js',
-  'first/second/index.js',
-  'third/index.js',
-  'first/fourth/index.js',
-];
+const expected = {
+  basic: [
+    'index.js',
+    'first/index.js',
+    'first/second/index.js',
+    'third/index.js',
+    'first/fourth/index.js',
+  ],
+  plus: [
+    'index.js',
+    'first/index.js',
+    'first/second/index.js',
+    'first/second/three/index.js',
+  ],
+};
 
-test('basic: should list all expected files', t => graph('./fixtures/graph/basic/index.js')
-  .then(result => t.same(expected, result)));
+test('basic: should list all expected files', t =>
+  graph('./fixtures/graph/basic/index.js')
+    .then(result => t.same(expected.basic, result)));
+
+test('basic plus: should list all expected files', t =>
+  graph('./fixtures/graph/basic-plus/index.js')
+    .then(result => t.same(expected.plus, result)));
 
 test('should reject on empty input', t =>
   t.throws(graph(), '`path` should be `String`, but got `Undefined`'));
