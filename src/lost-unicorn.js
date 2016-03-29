819f10224115a5e6288b65637ae76fa8b9ccf820
Vladimir Starkov <iamstarkov@gmail.com>
2016-03-29 17:48:59 +0200

    lost-unicorn: a bit of refactoring

--- src/lost-unicorn.js
+++ src/lost-unicorn.js
@@ -1,14 +1,16 @@
 import R from 'ramda';
 import diff from 'lodash.difference';
 
-const error = msg => { throw new TypeError(msg); };
-
-// isArrayOfString :: Input -> Boolean
-const isArrayOfStrings = R.both(R.is(Array), R.all(R.is(String)));
+// isArrayOfString :: Constructor -> Input -> Boolean
+const isArrayOf = R.curry((ctor, input) => R.both(R.is(Array), R.all(R.is(ctor)))(input));
 
 const lostUnicorn = (fs, graph) => R.pipe(
-  () => R.unless(isArrayOfStrings, () => error('`fs` should be an `Array[String]`, but got `' + R.type(fs) + '`'))(fs),
-  () => R.unless(isArrayOfStrings, () => error('`graph` should be an `Array[String]`, but got `' + R.type(graph) + '`'))(graph),
+  () => R.unless(isArrayOf(String), () => {
+    throw new TypeError(`\`fs\` should be an \`Array[String]\`, but got \`${R.type(fs)}\``);
+  })(fs),
+  () => R.unless(isArrayOf(String), () => {
+    throw new TypeError(`\`graph\` should be an \`Array[String]\`, but got \`${R.type(fs)}\``);
+  })(graph),
   () => diff(fs, graph)
 )(fs);
 
