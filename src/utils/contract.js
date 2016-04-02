c30f7e67ef70c70c52768253f3d26ca642253a3a
Vladimir Starkov <iamstarkov@gmail.com>
2016-04-02 02:26:22 +0200

    rewrite contract

--- src/utils/contract.js
+++ src/utils/contract.js
@@ -1,12 +1,14 @@
 import R from 'ramda';
 import { reject } from './promise-methods';
 
-// contract :: String -> Constructor -> a
-const contract = R.curry((name, ctor, param) => R.unless(
+// contract :: String -> Constructor -> cb -> a
+const contract = R.curry((name, ctor, cb, param) => R.unless(
   R.is(ctor),
-  () => reject(
+  () => cb(
     new TypeError(`\`${name}\` should be \`${R.type(ctor())}\`, but got \`${R.type(param)}\``)
   )
 )(param));
 
-export default contract;
+const thr = error => { throw error; };
+
+export { contract, thr, reject };
