8b43e14cb6739b85218f1239e9668d2fb9681841
Vladimir Starkov <iamstarkov@gmail.com>
2016-04-02 04:14:38 +0200

    wip

--- src/graph.js
+++ src/graph.js
@@ -1,12 +1,15 @@
 import R from 'ramda';
 import esDeps from 'es-deps';
-import contract from './utils/contract';
+import { contract, reject } from './utils/contract';
 import { log } from './utils/console-methods';
 import { all, resolve } from './utils/promise-methods';
-import { normalize, relative as _relative, join as _join, dirname } from 'path';
+import { relative as _relative, join as _join, dirname } from 'path';
+import { isModule, isLocalFile } from './is-module';
 
-const debug = msg => R.tap(R.partial(log, [`\n${msg}\n`]));
+const d = msg => R.tap(R.partial(log, [`${msg}`]));
 const { cwd } = process;
+
+const id = R.identity;
 
 const join = R.curryN(2, _join);
 const relative = R.curryN(2, _relative);
@@ -14,31 +17,37 @@ const relative = R.curryN(2, _relative);
 const resolveFile = R.pipe(
   join(cwd()),
   require.resolve,
-  relative(cwd())
+  relative(cwd()),
+  R.concat('./')
 );
 
 // chainP :: (Function -> [Promise]) -> [Promise]
-const chainP = R.curry((fn, iterableP) => {
-  return R.pipeP(resolve,
-    R.map(fn),
-    all,
-    R.unnest
-  )(iterableP);
-});
+const chainP = R.curry((fn, iterableP) => R.pipeP(resolve,
+  R.map(fn),
+  all,
+  R.unnest
+)(iterableP));
 
 const walk = R.curry((visited, file) => {
   const basedir = dirname(file);
-  const visitedAndCurrent = R.append(file, visited);
+  const resolveBase = R.pipe(join(basedir), resolveFile);
+  let visitedAndCurrent = R.append(file, visited);
   return R.unless(
     R.contains(R.__, visited),
     R.pipeP(resolve,
       esDeps,
-      R.map(R.pipe(
-        join(basedir),
-        resolveFile
-      )),
-      chainP(walk(visitedAndCurrent)),
-      R.prepend(file)
+      R.tap(deps => { log('\nfile', file, deps); }),
+      R.tap(R.map(R.when(isModule, item => {
+        visitedAndCurrent = R.append(item, visitedAndCurrent);
+      }))),
+      R.pipe(
+        R.filter(isLocalFile),
+        R.map(resolveBase),
+        chainP(walk(visitedAndCurrent)),
+        id
+      ),
+      R.prepend(file),
+      id
     )
   )(file);
 });
@@ -46,14 +55,14 @@ const walk = R.curry((visited, file) => {
 // graph :: String -> Promise Array[String]
 function graph(file) {
   return R.pipeP(resolve,
-    contract('file', String),
-    normalize,
+    contract('file', String, reject),
     resolveFile,
     walk([]),
     R.useWith(
       chainP,
       [R.pipe(resolveFile, dirname, relative), R.identity]
     )(file),
+    R.map(R.concat('./')),
     R.uniq
   )(file);
 }
