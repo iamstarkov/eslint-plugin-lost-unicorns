b6d8d0114c3ec8580ac1ad6f44400eaae25d67a0
Leonard Kinday <leonard@kinday.ru>
2016-04-03 11:53:23 +0300

    refactor `isLocalReg` and `isModule`

--- src/is-module.js
+++ src/is-module.js
@@ -1,19 +1,16 @@
 import R from 'ramda';
 import { contract } from './utils/contract';
 
-const isLocalReg = /\.|\//;
+const isLocalReg = /^[./]/;
 
 const isLocalFile = R.unary(R.pipe(
   contract('path', String),
-  R.take(1),
   R.test(isLocalReg)
 ));
 
-const isModule = R.unary(R.pipe(
-  contract('path', String),
-  R.take(1),
-  R.test(isLocalReg),
+const isModule = R.pipe(
+  isLocalFile,
   R.not
-));
+);
 
 export { isLocalFile, isModule };
