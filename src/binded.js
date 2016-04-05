b54a7b4a892165e4c2b591e8369b6507f004969e
Vladimir Starkov <iamstarkov@gmail.com>
2016-04-05 10:20:08 +0200

    fix strict mode

--- src/binded.js
+++ src/binded.js
@@ -1,5 +1,7 @@
 import R from 'ramda';
 
+const strictModeIncompatibleProps = ['caller', 'callee', 'arguments'];
+
 const bind = input => (state, name) =>
   R.merge(state, { [name]: input[name].bind(input) });
 
@@ -9,6 +11,7 @@ function binded(input) {
   }
   return R.pipe(
     Object.getOwnPropertyNames,
+    R.without(strictModeIncompatibleProps),
     R.filter(R.pipe(
       R.prop(R.__, input),
       R.is(Function)
