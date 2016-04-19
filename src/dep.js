bb5983ae5311d88d0af932dbdc4e3a3d177cf351
Vladimir Starkov <iamstarkov@gmail.com>
2016-04-19 12:40:47 +0200

    dep: add explanatory comments

--- src/dep.js
+++ src/dep.js
@@ -3,14 +3,22 @@ import { join } from 'path';
 import d from './utils/debug';
 
 const { cwd } = process;
+const id = R.identity;
 
 const joinNullCwd = (inPathArr, file) =>
   R.ifElse(
     R.is(String),
     R.pipe(
+      // d('debug'), move it around to see whats in the output at current state in the pipe
       R.append(R.__, inPathArr),
+      data => {
+        // example function how to operate in non point-free mode
+        const newData = data.map(item => item);
+        return newData;
+      },
       R.prepend(cwd()),
-      R.apply(join)
+      R.apply(join),
+      id
     ),
     R.always(null))(file);
 
