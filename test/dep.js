7c53aa3b8b07ac54eaef21d1b91c8689f04eb71b
Vladimir Starkov <iamstarkov@gmail.com>
2016-04-19 12:29:37 +0200

    dep: write tests for not messing up with absolute paths

--- test/dep.js
+++ test/dep.js
@@ -37,6 +37,21 @@ test('in path', t => t.deepEqual(
     resolved: join(cwd(), 'meow', 'purr', './basic/first/second/index.js') }
 ));
 
+test.skip('dont messup absolute paths', t => {
+  // change `.skip` to `.only` to run only this test case
+  // after fixing this one, remove `.only` to verify that nothing else in this file is broken
+  // use `path.isAbsolute` to skip `join.apply([cwd(), [inPathArr], file].flatten())``
+  // @see https://nodejs.org/api/path.html#path_path_isabsolute_path
+  t.deepEqual(
+    dep([], null, '/global/file.js', null).from,
+    '/global/file.js'
+  );
+  t.deepEqual(
+    dep([], null, null, '/global/file.js').resolved,
+    '/global/file.js'
+  );
+});
+
 test('in path curried', t => t.deepEqual(
   dep(['meow', 'purr'])(null, null, './basic/first/second/index.js'),
   { requested: null,
